e9384b32d25fe9996e4294c089cedc9ec9b09a29
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-06 10:08:34 -0400

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -10,12 +10,13 @@ public class Alien extends Rectangle{
 
   public static int yspeed; //move speed in vertical direction
   public static int xspeed; //move speed in horizontal direction
-  public static final int ALIEN_WIDTH = 20; //size of Alien
+  public static final int ALIEN_WIDTH = 27; //size of Alien
+  public static final int ALIEN_HEIGHT = 20;
   public boolean dead = false;
   
   //constructor creates the Alien
   public Alien(int x, int y){
-    super(x, y, ALIEN_WIDTH, ALIEN_WIDTH);
+    super(x, y, ALIEN_WIDTH, ALIEN_HEIGHT);
     yspeed = 0;
     xspeed = 0;
   }
@@ -35,7 +36,7 @@ public class Alien extends Rectangle{
   //Replace ball with image of alien later?
   public void draw(Graphics g){
     g.setColor(Color.white); //set the colour
-    g.fillOval(x, y, ALIEN_WIDTH, ALIEN_WIDTH); //draw with given position and dimensions
+    g.fillOval(x, y, ALIEN_WIDTH, ALIEN_HEIGHT); //draw with given position and dimensions
   }
 
 }
