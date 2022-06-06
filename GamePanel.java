08400e2d063c7ec78e2c181032cecf77eaf936b8
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-06 09:32:06 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -5,31 +5,32 @@ import javax.swing.*;
 public class GamePanel extends JPanel implements Runnable, KeyListener{
 
   public static final int GAME_WIDTH = 1000;
-  public static final int GAME_HEIGHT = 750;
+  public static final int GAME_HEIGHT = 650;
   
   public Thread gameThread;
   public Image image;
   public Graphics graphics;
   public Alien[][] aliens = new Alien[11][5];
   public PlayerShip ship;
-  public Menu start;
+  public Title start;
   public static long startTime; //when game starts, set starttime to that time
   public Score score;
+  public int counter = 1;
   
   public GamePanel(){
     int x, y = 20; // coordinates for setting each alien position
   
-    ship = new PlayerShip(GAME_WIDTH/2 - PlayerShip.WIDTH / 2, PlayerShip.YCOORD);
-    start = new Menu(GAME_WIDTH, GAME_HEIGHT);
+    ship = new PlayerShip(GAME_WIDTH/2 - PlayerShip.WIDTH / 2, 550);
+    start = new Title(GAME_WIDTH, GAME_HEIGHT);
     score = new Score(GAME_WIDTH, GAME_HEIGHT);
     
     for(int i = 0; i < 11; ++i){
-      x = 20; // change to the first alien position later(make it so its centered)
+      x = 100; // change to the first alien position later(make it so its centered)
     
       for(int j = 0; j < 5; ++j){
         //intialization of each alien object
         aliens[i][j] = new Alien(x, y);
-        x += 50;
+        x += 100;
       }
       
       y += 40; // next row of aliens
@@ -62,7 +63,10 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
           }
         }
       }
-      ship.draw();
+      ship.draw(g);
+      for (int i = 0; i < 5; i++) {
+    	  ship.bullets[i].draw(g);
+      }
     }
   }
 
@@ -72,6 +76,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
         for(int j = 0; j < 5; ++j){
         	aliens[i][j].move();
         }
+    }
+    for (int i = 0; i < 5; i++) {
+  	  ship.bullets[i].move();
     }
   }
 
@@ -89,28 +96,41 @@ public class GamePanel extends JPanel implements Runnable, KeyListener{
     //checks if projectile hits alien, then makes alien[i][j].dead = true, increase score
     for(int i = 0; i < 11; ++i){
         for(int j = 0; j < 5; ++j){
+        	/*
           if(aliens[i][j].intersects(projectile)){ //change the projectile name to the name of the projectile object later
             aliens[i][j].dead = true;
             score.score += 100;
           }
+          */
         }
       }
       
     //checks if projectile hits house/wall
     
     //checks if projectile hits player ship
-    if(projectile.intersects(ship)){ //change the projectile name to the name of the projectile object later
+    ///if(projectile.intersects(ship)){ //change the projectile name to the name of the projectile object later
        //add code here that decreases number of lives
        //also add code that if number of lives = 0, then display the end screen and stop game
-    }
+    //}
+    
     
     //checks if farthest alien to one side hits wall, then reverses the x direction and moves all aliens down, as aliens reach more row, speed increases
+    /*
+    if((System.nanoTime() - startTime) >= 1000000000 * counter) {
+    	counter += 5;
+    	Alien.xspeed = 1;
+    }
+    else {
+    	Alien.xspeed = 0;
+    }
+    */
+    
     if(aliens[0][4].x >= GAME_WIDTH - Alien.ALIEN_WIDTH) {
-    	Alien.xspeed *= -1;
+    	Alien.xspeed = -1;
     }
     
     if(aliens[0][0].x <= 0) {
-    	Alien.xspeed *= -1;
+    	Alien.xspeed = -1;
     }
     
     //add check that if all aliens dead, then resets wave(i.e. make aliens[i][j].dead == false and reset all the x y positions to the original position)
