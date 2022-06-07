c05ccd5850da82d46f4b2e5ef0d56c8becb86c8c
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-07 09:36:26 -0400

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -5,6 +5,10 @@ Child of Rectangle because that makes it easy to draw and check for collision
 In 2D GUI, basically everything is a rectangle even if it doesn't look like it!
 */ 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Alien extends Rectangle{
 
@@ -13,12 +17,14 @@ public class Alien extends Rectangle{
   public static final int ALIEN_WIDTH = 27; //size of Alien
   public static final int ALIEN_HEIGHT = 20;
   public boolean dead = false;
+  public Image img;
   
   //constructor creates the Alien
-  public Alien(int x, int y){
+  public Alien(int x, int y) throws IOException{
     super(x, y, ALIEN_WIDTH, ALIEN_HEIGHT);
     yspeed = 0;
     xspeed = 0;
+    img = ImageIO.read(new File("C:\\Users\\334799608\\Downloads\\alien.png"));
   }
 
   //update the position of the ball (called from GamePanel frequently)
@@ -34,9 +40,10 @@ public class Alien extends Rectangle{
 
   //draw the obstacle to the screen (called from GamePanel frequently)
   //Replace ball with image of alien later?
-  public void draw(Graphics g){
+  public void draw(Graphics g) {
     g.setColor(Color.white); //set the colour
-    g.fillOval(x, y, ALIEN_WIDTH, ALIEN_HEIGHT); //draw with given position and dimensions
+    //g.fillOval(x, y, ALIEN_WIDTH, ALIEN_HEIGHT); //draw with given position and dimensions
+    g.drawImage(img, x, y, 27, 20, null);
   }
 
 }
