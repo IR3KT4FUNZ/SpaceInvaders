b09d896487d239cee9a54536db0ecbb42d4dbb0b
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-08 09:35:39 -0400

    Update House.java

--- House.java
+++ House.java
@@ -1,14 +1,14 @@
 import java.awt.*;
 
 public class House extends Rectangle {
-  public static int width = 20; //width of the house
-  public static int height = 16; //height of the house
+  public static int width = 60; //width of the house
+  public static int height = 48; //height of the house
   public boolean[][] alive; //array for squares of the house
   
   //constructor, defines walls
   public House(int x, int y) {
     super(x, y, width, height);
-    alive = new boolean[10][8];
+    alive = new boolean[15][12];
     for (int i = 0; i < 10; i++) {
       for (int j = 0; j < 8; j++) {
         alive[i][j] = true;
@@ -16,14 +16,21 @@ public class House extends Rectangle {
     }
     alive[0][0] = false;
     alive[0][1] = false;
+    alive[0][2] = false;
     alive[1][0] = false;
-    alive[8][0] = false;
-    alive[9][0] = false;
-    alive[9][1] = false;
-    alive[4][4] = false;
-    alive[5][4] = false;
-    for (int i = 3; i <= 6; i++) {
-      for (int j = 5; j <= 7; j++) {
+    alive[1][1] = false;
+    alive[2][0] = false;
+    alive[12][0] = false;
+    alive[13][0] = false;
+    alive[13][1] = false;
+    alive[14][0] = false;
+    alive[14][1] = false;
+    alive[14][2] = false;
+    for (int i = 5; i <= 9; i++) {
+      alive[i][7] = false; 
+    }
+    for (int i = 4; i <= 10; i++) {
+      for (int j = 8; j <= 11; j++) {
         alive[i][j] = false; 
       }
     }
@@ -31,11 +38,11 @@ public class House extends Rectangle {
   
   //display the alive parts of the defensive house
   public void draw(Graphics g) {
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 8; j++) {
+    for (int i = 0; i < 15; i++) {
+      for (int j = 0; j < 12; j++) {
         if (alive[i][j]) {
           g.setColor(Color.red);
-          g.fillRectangle(x + i * 2, y + j * 2, 2, 2);
+          g.fillRectangle(x + i * 4, y + j * 4, 4, 4);
         }
       }
     }
