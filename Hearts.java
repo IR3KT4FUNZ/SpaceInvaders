b4a1fbde72f0ff7b9e364525a31c035d63f8e135
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-14 09:37:32 -0400

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -7,23 +7,28 @@ import java.io.*;
 
 public class Hearts extends Rectangle {
   public Image img;
-  public final WIDTH = 30;
-  public final HEIGHT = 30;
-  boolean alive;
+  public static final int WIDTH = 30;
+  public static final int HEIGHT = 30;
+  public static int lives = 3;
 
   public Hearts(int x, int y) throws IOException {
     super(x, y, WIDTH, HEIGHT);
-    img = ImageIO.read(new File("C:\\Users\\334799608\\Downloads\\ship2.png");
+    img = ImageIO.read(new File("C:\\Users\\334799608\\Downloads\\ship2.png"));
     alive = true;
   }
   
-  public void setDead() {
-    alive = false;
-  }
-  
   public void draw(Graphics g) {
-    if (alive) {
-      g.drawImage(img, x, y, WIDTH, HEIGHT, null);
+    if(lives == 3) {
+      g.drawImage(img, 900, 10, WIDTH, HEIGHT, null);
+      g.drawImage(img, 930, 10, WIDTH, HEIGHT, null);
+      g.drawImage(img, 960, 10, WIDTH, HEIGHT, null);
+    }
+    if(lives == 2) {
+      g.drawImage(img, 930, 10, WIDTH, HEIGHT, null);
+      g.drawImage(img, 960, 10, WIDTH, HEIGHT, null);
+    }
+    if(lives == 1) {
+      g.drawImage(img, 960, 10, WIDTH, HEIGHT, null);
     }
   }
 }
