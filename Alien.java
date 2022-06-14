81a5bcb37d20a6a2bc69ebb95c2bff4b458b3f5a
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-14 19:19:01 -0400

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,23 +1,18 @@
-/* Alien class defines behaviours for the Alien
-
-Child of Rectangle because that makes it easy to draw and check for collision
-
-In 2D GUI, basically everything is a rectangle even if it doesn't look like it!
-*/ 
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
+//Class for aliens in game
+//Eric Wang, Elliot Ngo, 6/14/2022
 
 public class Alien extends Rectangle{
 
   public static int yspeed; //move speed in vertical direction
   public static int xspeed; //move speed in horizontal direction
-  public static final int ALIEN_WIDTH = 27; //size of Alien
-  public static final int ALIEN_HEIGHT = 20;
-  public boolean dead = false;
-  public Image img;
+  public static final int ALIEN_WIDTH = 27; //width of Alien
+  public static final int ALIEN_HEIGHT = 20; //height of alien
+  public boolean dead = false; //whether alien has been killed
+  public Image img; //image of alien
   
   //constructor creates the Alien
   public Alien(int x, int y) throws IOException{
@@ -27,23 +22,21 @@ public class Alien extends Rectangle{
     img = ImageIO.read(new File("alien.png"));
   }
 
-  //update the position of the ball (called from GamePanel frequently)
+  //update the position of the ball
   public void move(){
     x = x + xspeed;
     y = y + yspeed;
   }
   
+  //make the alien shoot a bullet at the player
   public void shoot(int i, Projectile[] alienBullets) {
     alienBullets[i].x = this.x + 13;
     alienBullets[i].y = this.y + 20;
     alienBullets[i].setYVelocity(8);
   }
 
-  //draw the obstacle to the screen (called from GamePanel frequently)
-  //Replace ball with image of alien later?
+  //draw the alien to the screen
   public void draw(Graphics g) {
-    g.setColor(Color.white); //set the colour
-    //g.fillOval(x, y, ALIEN_WIDTH, ALIEN_HEIGHT); //draw with given position and dimensions
     g.drawImage(img, x, y, 27, 20, null);
   }
 
