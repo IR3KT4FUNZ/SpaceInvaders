35b79842aef275eb3b80ab764e6de717f243e93f
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-14 09:17:47 -0400

    Create Hearts.java

---
+++ Hearts.java
@@ -0,0 +1,29 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+import javax.swing.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class Hearts extends Rectangle {
+  public Image img;
+  public final WIDTH = 30;
+  public final HEIGHT = 30;
+  boolean alive;
+
+  public Hearts(int x, int y) throws IOException {
+    super(x, y, WIDTH, HEIGHT);
+    img = ImageIO.read(new File("C:\\Users\\334799608\\Downloads\\heart.png");
+    alive = true;
+  }
+  
+  public void setDead() {
+    alive = false;
+  }
+  
+  public void draw(Graphics g) {
+    if (alive) {
+      g.drawImage(img, x, y, WIDTH, HEIGHT, null);
+    }
+  }
+}
