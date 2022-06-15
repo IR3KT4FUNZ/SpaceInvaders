a92476530bbac19c0eb0c4322c3db8c2bad49e5e
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-14 21:11:47 -0400

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -3,16 +3,19 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+//Projectile class for space invaders
+//Eric Wang, Elliot Ngo, 6/14/2022
+
 public class Projectile extends Rectangle {
 
   public int xVelocity; //horizontal velocity of projectile
   public int yVelocity; //vertical velocity of projectile
   public static final int LENGTH = 10; //dimension of square projectile
   public static final int WIDTH = 25; //dimension of square projectile
-  public Image bullet3;
-  public Image bullet4;
+  public Image bullet3; //one image used for sprite
+  public Image bullet4; //second image used for sprite
   
-  //constructor creates projectile as a 2x2 square with velocity
+  //constructor creates projectile as a rectangle with velocity
   public Projectile(int x, int y, int xVel, int yVel) throws IOException {
     super(x, y, LENGTH, WIDTH);
     xVelocity = xVel;
@@ -21,12 +24,12 @@ public class Projectile extends Rectangle {
     bullet4 = ImageIO.read(new File("bullet4.png"));
   }
   
-  //helper method
+  //helper method to change velocity
   public void setYVelocity(int yVel) {
     yVelocity = yVel;
   }
   
-  //helper method
+  //helper method to change velocity
   public void setXVelocity(int xVel) {
     xVelocity = xVel;
   }
