8f88ee1a92941361879572829239df29d242944e
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-15 10:12:18 -0400

    Update Endscreen.java

--- Endscreen.java
+++ Endscreen.java
@@ -17,7 +17,7 @@ public class Endscreen {
   
   //check for restart input
   public void keyPressed(KeyEvent e){
-	 if(e.getKeyChar() == 'r' && display){ // checks if g key is pressed to continue
+	 if(e.getKeyChar() == 'r'){ // checks if r key is pressed to continue
 	   Title.check = false;
    	   display = false;
 	 }
