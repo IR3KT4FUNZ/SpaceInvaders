1e274655579d2e98788d844f6bba76538555fdc1
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-14 21:06:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 import java.io.IOException;
 
+//Main class for space invaders
+//Eric Wang, Elliot Ngo, 6/14/2022
+
 class Main {
   public static void main(String[] args) throws IOException {
 
-    new GameFrame();
+    new GameFrame(); //initializes the game
     
   }
 }
