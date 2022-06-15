125adba4ae785e485c22d80b6587654d4ef2718e
IR3KT4FUNZ <72675783+IR3KT4FUNZ@users.noreply.github.com>
2022-06-14 21:05:11 -0400

    Update Hearts.java

--- Hearts.java
+++ Hearts.java
@@ -5,17 +5,22 @@ import javax.swing.*;
 import javax.imageio.*;
 import java.io.*;
 
-public class Hearts extends Rectangle {
-  public Image img;
-  public static final int WIDTH = 30;
-  public static final int HEIGHT = 30;
-  public static int lives = 3;
+//Lives display for space invaders
+//Eric Wang, Elliot Ngo, 6/14/2022
 
+public class Hearts extends Rectangle {
+  public Image img; //display image for hearts
+  public static final int WIDTH = 30; //width of image
+  public static final int HEIGHT = 30; //height of image
+  public static int lives = 3; //number of lvies
+
+  //constructor, initialized \ at x, y coordinates
   public Hearts(int x, int y) throws IOException {
     super(x, y, WIDTH, HEIGHT);
     img = ImageIO.read(new File("ship2.png"));
   }
   
+  //display as many lives as the player has
   public void draw(Graphics g) {
     if(lives == 3) {
       g.drawImage(img, 900, 10, WIDTH, HEIGHT, null);
