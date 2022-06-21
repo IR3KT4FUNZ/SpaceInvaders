c1166a7f2cb866673e265cc8be4c4a8600593107
ElliotNo <106601553+ElliotNo@users.noreply.github.com>
2022-06-20 22:25:17 -0400

    Add files via upload

---
+++ RunMe.bat
@@ -0,0 +1,17 @@
+javac -target 1.8 -source 1.8 Main.java
+javac -target 1.8 -source 1.8 Background.java
+javac -target 1.8 -source 1.8 Alien.java
+javac -target 1.8 -source 1.8 Endscreen.java
+javac -target 1.8 -source 1.8 GameFrame.java
+javac -target 1.8 -source 1.8 GamePanel.java
+javac -target 1.8 -source 1.8 Hearts.java
+javac -target 1.8 -source 1.8 House.java
+javac -target 1.8 -source 1.8 Instructions.java
+javac -target 1.8 -source 1.8 Music.java
+javac -target 1.8 -source 1.8 PlayerShip.java
+javac -target 1.8 -source 1.8 Projectile.java
+javac -target 1.8 -source 1.8 Score.java
+javac -target 1.8 -source 1.8 Title.java
+pause
+java Main
+pause
